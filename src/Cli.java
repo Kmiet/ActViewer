bfb775c5ba7f9ff8edf22cf95bf3998f75ac5b99
Kmiet <N/A>
2017-12-15 02:28:52 +0100

    OptionParser added

--- src/Cli.java
+++ src/Cli.java
@@ -9,6 +9,52 @@ public class Cli {
     private String points;
     private String lines;
 
+    private Options setOptions(){
+        Options options = new Options();
+        options.addOption("f",true,"Path to file");
+        options.addOption("h", false, "Show help");
+        options.addOption("c",true,"Show chapter [number of chapter]");
+        options.addOption("a",true,"Number of article");
+        options.addOption("s",true,"Number of section");
+        options.addOption("p",true,"Number of point");
+        options.addOption("l",true,"Number of line/letter");
+
+        return options;
+    }
+
+    public void parse(String[] arguments) throws ParseException {
+
+        Options options = this.setOptions();
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, arguments);
+
+        if(cmd.hasOption("f")){
+            this.filename = cmd.getOptionValue("f");
+        } else{
+            throw new ParseException("Has to run with option: f");
+        }
+
+        if(cmd.hasOption("c")){
+            this.chapters = cmd.getOptionValue("c");
+        }
+
+        if(cmd.hasOption("a")){
+            this.articles = cmd.getOptionValue("a");
+        }
+
+        if(cmd.hasOption("s")){
+            this.sections = cmd.getOptionValue("s");
+        }
+
+        if(cmd.hasOption("p")){
+            this.points = cmd.getOptionValue("p");
+        }
+
+        if(cmd.hasOption("l")){
+            this.lines = cmd.getOptionValue("l");
+        }
+    }
+
     public String getFilename() {
         return filename;
     }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@
 public class Main {
 
     public static void main(String[] args){
-        
+        try {
+            Cli config = new Cli();
+            config.parse(args);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
